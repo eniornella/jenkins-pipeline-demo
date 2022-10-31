e742b17572c76f984d92b57abc4f7d649d1c8750
eni.ornella <eni.ornella2@gmail.com>
2022-10-31 16:37:15 -0400

    commiting terraformfiles

---
+++ var.tf
@@ -0,0 +1,10 @@
+# Below is the variables blocks
+variable "ami_id" {
+  default = "ami-09d3b3274b6c5d4aa"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "ec2_name_tag" {
+  default = "webserver"
+}
